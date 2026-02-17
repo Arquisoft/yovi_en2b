460dac997328d75a9ba15357afb735dcf302853b
losadgm <uo295106@uniovi.es>
2026-02-17 21:36:07 +0100

    fix: sonar complaining about harcoded passwd

--- webapp/src/RegisterForm.tsx
+++ webapp/src/RegisterForm.tsx
@@ -28,7 +28,7 @@ const RegisterForm: React.FC = () => {
         body: JSON.stringify({
           username,
           email: `${username.toLowerCase()}@test.com`,
-          password: 'Password123!'
+          password: import.meta.env.VITE_TEST_PASSWORD ?? 'DefaultPass123!'
         })
       });
 
--- webapp/src/env.d.ts
+++ webapp/src/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_TEST_PASSWORD: string
   readonly VITE_API_URL?: string
 }
 
