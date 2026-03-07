6e328f9880ea5f4f2269ccd7b21268c0bce570ca
Marcos Losada García <158076748+losadgm@users.noreply.github.com>
2026-03-07 11:32:06 +0100

    feat(nginx): seems to be working now! (#82)

---
+++ webapp/nginx/start-nginx.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+ENV=${NODE_ENV:-production}
+
+echo "Starting nginx server with env: $ENV"
+
+if [ "$ENV" = "production" ]; then
+    cp /etc/nginx/prod.conf /etc/nginx/conf.d/prod.conf
+else
+    cp /etc/nginx/dev.conf /etc/nginx/conf.d/dev.conf
+fi
+
+exec /docker-entrypoint.sh nginx -g "daemon off;"
